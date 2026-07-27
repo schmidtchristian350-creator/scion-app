c79085c14d4a272fc88e92ebdcfba458c17a1f0d
schmidtchristian350-creator <schmidtchristian350@gmail.com>
2026-07-27 12:19:59 +0200

    Update webseite.py

--- webseite.py
+++ webseite.py
@@ -2,7 +2,7 @@ import streamlit as st
 from openai import OpenAI
 
 # Dein echter, neuer Schlüssel ist hier hinterlegt
-client = OpenAI(api_key="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")
+client = OpenAI(api_key="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")
 
 st.title("Scion Mind")
 st.markdown("*designed by Christian Schmidt*")
